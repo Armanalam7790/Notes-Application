93038c04aac85c759e91d92b8c09c38ee8df4bec
Armanalam7790 <alamarman7790@gmail.com>
2026-05-27 20:03:59 +0530

    fix dns mongodb

--- third/src/config/databse.js
+++ third/src/config/databse.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
-
+import dns from 'dns'
+dns.setServers(['8.8.8.8', '8.8.4.4'])
 let connectDB = async()=>{
     try {
         await mongoose.connect('mongodb+srv://todo:todo12345@cluster0.pqscxrt.mongodb.net/')
         console.log('mongodb connected');
         
     } catch (error) {
-        console.log('mongodb connection failed');
+        console.log('mongodb connection failed', error);
         
     }
 }
