27caf52e2286980b8ccf53bea5efad07818e4d6a
Armanalam7790 <alamarman7790@gmail.com>
2026-05-27 12:54:56 +0530

    create notes update features

--- first/src/app.js
+++ first/src/app.js
@@ -67,4 +67,46 @@ app.get('/api/notes',async(req, res)=>{
 })
 
 
+/**
+ * @route  patch /api/note/:id
+ * @descriprion  description update notes
+ * @access public
+ */
+
+
+app.patch('/api/notes/:id',async(req,res)=>{
+     let {id} = req.params
+     let {description} = req.body
+
+     if (!description) {
+    return res.status(400).json({
+      error:"description is required"
+    })
+     }
+     if (description.trim().length<10) {
+       return res.status(400).json({
+       error:"description is must mme at least 10 char long"
+    })
+     }
+
+
+     let notes  =  await NotesModel.findById(id)
+
+     if (!notes) {
+      return  res.status(404).json({
+        error:"notes not found"
+      })
+     }
+   notes.description =  description
+     await notes.save()
+
+
+     return res.status(200).json({
+      message:"description update",
+      notes
+     })
+
+  
+})
+
 export default  app
